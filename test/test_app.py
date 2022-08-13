029a84b83f0e63103774a8c7f3b8376a66481c89
veronica1012 <111160893+veronica1012@users.noreply.github.com>
2022-08-12 20:56:45 -0500

    Add files via upload

--- test/test_app.py
+++ test/test_app.py
@@ -1,1 +1,16 @@
+from app import get_price
 
+
+def test_get_price():
+    cocacola_price = get_price('KO').json
+    print(cocacola_price)
+
+    assert cocacola_price['price'] > 0
+    assert cocacola_price['name'] == 'The Coca-Cola Company'
+    assert cocacola_price['exchange'] == 'NYSE'
+    assert cocacola_price['currency'] == 'USD'
+
+    assert get_price('KSLAFSADF').status_code == 404
+
+
+test_get_price()
